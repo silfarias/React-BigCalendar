856a742af913027519595069a65796d06955507e
silfarias <silvifarias17@gmail.com>
2024-06-05 11:06:57 -0300

    feat: se agregaron propiedades del calendario

--- react-big-calendar/src/App.jsx
+++ react-big-calendar/src/App.jsx
@@ -1,10 +1,35 @@
 import { Calendar, dayjsLocalizer } from 'react-big-calendar'
+import { CiCalendarDate } from "react-icons/ci";
 import "react-big-calendar/lib/css/react-big-calendar.css"
 import dayjs from 'dayjs'
+import 'dayjs/locale/es'
+import './Calendar.css'
 
+dayjs.locale('es')
 
 function App () {
   const localizer = dayjsLocalizer(dayjs)
+  const events = [
+    {
+      start: dayjs('2024-06-05T09:00').toDate(),
+      end: dayjs('2024-06-05T11:00').toDate(),
+      title: "Python CD",
+    }
+  ]
+
+  const components = {
+    event: props => {
+      console.log(props)
+      return <div style={{
+        display: 'flex',
+        flexDirection: 'row',
+        gap: '6px',
+      }}>
+        <CiCalendarDate />
+        {props.title}
+      </div>
+    }
+  }
   return (
     <div style={
       {
@@ -14,6 +39,26 @@ function App () {
     }>
       <Calendar
         localizer={localizer}
+        events={events}
+        views={["month", "week", "day", "agenda"]}
+        defaultView='month'
+        toolbar={true}
+        min={dayjs('2024-06-05T09:00').toDate()}
+        max={dayjs('2024-06-05T17:00').toDate()}
+        formats={{
+          dayHeaderFormat: date => {
+            return dayjs(date).format('dddd h:mm A')
+          }
+        }}
+        components={components}
+        messages={{
+          next: "Siguiente",
+          previous: "Anterior",
+          today: "Hoy",
+          month: "Mes",
+          week: "Semana",
+          day: "Día",
+        }}
       />
     </div>
   )
